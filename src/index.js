0b8592843f2adbdeba181a9e4585fa9c83c682a6
Anastasiya957 <anastasiyabarysheva95@gmail.com>
2018-02-28 00:39:24 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// your solution
+	var openBrackets = [];
+	var closeBrackets = [];
+
+	for (var i = 0; i < bracketsConfig.length; i++) {
+		openBrackets[i] = bracketsConfig[i][0];
+		closeBrackets[i] = bracketsConfig[i][1];
+	}
+
+	var stack = [];
+	
+	for (var i = 0; i < str.length; i++) {
+
+		if ( openBrackets.indexOf(str[i]) != -1 && closeBrackets.indexOf(str[i]) == -1 ) {
+			stack.push(str[i]);
+		}
+
+		else if ( openBrackets.indexOf(str[i]) != -1 ) {
+
+			if (stack.length !=0 && str[i] == stack[ stack.length - 1 ]) {
+				stack.pop();
+			} else {
+				stack.push(str[i]);
+			}
+		} else {
+
+			if ( stack.length == 0 ) {
+				return false;
+			}
+			posOpenBr = openBrackets.indexOf( stack[stack.length - 1] );
+			posCloseBr = closeBrackets.indexOf( str[i] );
+
+			if ( posOpenBr == posCloseBr ) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+
+	if (stack.length == 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
